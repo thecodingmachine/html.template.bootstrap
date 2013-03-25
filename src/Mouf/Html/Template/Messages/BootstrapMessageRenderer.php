1d4a46cfaae7b95396c51dc9dd0838feaf4f487d
Marc Teyssier <m.teyssier@thecodingmachine.com>
2013-03-25 09:42:47 +0100

    Fix bug on renderer

--- src/Mouf/Html/Template/Messages/BootstrapMessageRenderer.php
+++ src/Mouf/Html/Template/Messages/BootstrapMessageRenderer.php
@@ -6,6 +6,8 @@
 */
 namespace Mouf\Html\Template\Messages;
 
+use Mouf\Html\Widgets\MessageService\Widget\MessageRendererInterface;
+
 use Mouf\Html\Widgets\MessageService\Service\UserMessageInterface;
 
 /**
@@ -13,21 +15,21 @@ use Mouf\Html\Widgets\MessageService\Service\UserMessageInterface;
  * @author Marc Teyssier
  *
  */
-class DefaultMessageRenderer implements MessageRendererInterface {
+class BootstrapMessageRenderer implements MessageRendererInterface {
 	/**
 	 * (non-PHPdoc)
 	 * @see \Mouf\Html\Widgets\MessageService\Widget\MessageRendererInterface::render()
 	 */
-	function render(UserMessageInterface $message) {
+	function render(UserMessageInterface $message, $nbRepeat) {
 
 		$html = $message->getMessage();
 		$type = $message->getType();
 		
 		echo "<div class='alert alert-".$type."'>";
 		echo $html;
-		if ($invertedMessages[$html]["nbOccurences"] > 1) {
+		if ($nbRepeat > 1) {
 			// TODO: translate this.
-			echo " (message repeated ".$invertedMessages[$html]["nbOccurences"]." times)";
+			echo " (message repeated ".$nbRepeat." times)";
 		}
 		echo "</div>";
 	}
