446e2f75c7d9bfbfd8aba0dad2e2ec0213651575
David Négrier <d.negrier@thecodingmachine.com>
2014-04-29 13:45:33 +0200

    Making Bootstrap NavBar renderable

--- src/Mouf/Html/Template/Menus/BootstrapNavBar.php
+++ src/Mouf/Html/Template/Menus/BootstrapNavBar.php
@@ -9,6 +9,7 @@ namespace Mouf\Html\Template\Menus;
 use Mouf\Html\Widgets\Menu\MenuInterface;
 use \Mouf\Html\HtmlElement\HtmlElementInterface;
 use \Mouf\Html\Widgets\Menu\MenuItemInterface;
+use Mouf\Html\Renderer\Renderable;
 
 /**
  * This class is in charge of rendering a Bootstrap navbar.
@@ -18,6 +19,8 @@ use \Mouf\Html\Widgets\Menu\MenuItemInterface;
  * @Component
  */
 class BootstrapNavBar implements HtmlElementInterface {
+	
+	use Renderable;
 	
 	/**
 	 * The elements of the navbar
@@ -77,35 +80,6 @@ class BootstrapNavBar implements HtmlElementInterface {
 	public function __construct($children = array()) {
 		$this->children = $children;
 	}
-	
-	public function toHtml() {
-		if(!$this->allWidth) {
-			echo '<div class="container">';
-		}
-		echo '<div class="navbar '.($this->allWidth?' navbar-static-top':'').($this->fixed?' navbar-fixed-'.$this->fixed:'').($this->inverted?' navbar-inverse':' navbar-default').'" role="navigation">';
-		if($this->allWidth) {
-			echo '<div class="container">';
-		}
-		echo '<div class="navbar-header">';
-		echo '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">';
-		echo '<span class="sr-only">Toggle navigation</span>';
-		echo '<span class="icon-bar"></span>';
-		echo '<span class="icon-bar"></span>';
-		echo '<span class="icon-bar"></span>';
-		echo '</button>';
 
-		if ($this->title) {
-			echo '<a class="navbar-brand" href="'.ROOT_URL.$this->titleLink.'">'.$this->title.'</a>';
-		}
-		echo '</div>';
-		echo '<div class="collapse navbar-collapse navbar-ex1-collapse">';
-		foreach ($this->children as $child) {
-			$child->toHtml();
-		}
-		
-		echo '</div>';
-		echo '</div>';
-		echo '</div>';
-	}
 }
 ?>
