14636e9f218defee68afeddc16e017340db870ea
kevin <k.nguyen@thecodingmachine.com>
2020-11-30 14:27:30 +0100

    fix potential warning if headers have been sent

--- src/Mouf/Html/Template/BootstrapTemplate.php
+++ src/Mouf/Html/Template/BootstrapTemplate.php
@@ -123,7 +123,7 @@ class BootstrapTemplate extends BaseTemplate
         // Let's register the template renderer in the default renderer.
         $this->getDefaultRenderer()->setTemplateRendererInstanceName($this->getTemplateRendererInstanceName());
 
-        if (PHP_SAPI !== 'cli') {
+        if (PHP_SAPI !== 'cli' && !headers_sent()) {
             header('Content-Type: text/html; charset=utf-8');
         }
         //Renderable::toHtml();// __DIR__."/../../../../views/template.php";
